d93bf3b676a2ce8e089b5c63d5f90972cf91767f
DianaSvetlova <dianasvetlova26@gmail.com>
2025-01-10 21:17:48 +0300

    js types

---
+++ lesson11/index.js
@@ -0,0 +1,151 @@
+// // let b = 1000
+// // console.log(b);
+// // // a = 500
+// // // console.log(a);
+
+// // // console.log(a);
+
+
+// // // let a = 500
+// // // console.log(a);
+// // // a=Infinity
+// // // a=-1/0
+// // // console.log(a);
+
+// // const c1 = 42
+// // console.log(c1);
+
+// // console.log(0/0);
+
+// // console.log(-12545/0);
+// // console.log(1237454/0);
+
+// // console.log('a'*1);
+
+// // // let str = 'hjg "hm" ngbm'
+// // // console.log(str);
+
+// // // let str = `1 + 1 = ${1+2}`
+// // // console.log(str);
+
+// // const myName = 'Dina!'
+// // console.log(`my name is ${myName}`);
+
+// // // alert('message')
+// // // console.log('console message');
+
+// // // confirm ('please')
+
+// // // prompt('smth', 'Dina')
+
+// // // const nickName = prompt('Enter y name', 'Dina')
+// // // console.log(`hello, ${nickName}`);
+
+// // let a = null
+// // console.log(a);
+
+// // a=2
+// // console.log(a);
+
+// const person = {
+//     firstName: 'Dina',
+//     lastName: 'Vi',
+//     age: 36,
+//     edu: {
+//         secondary: {
+//             name: 'fgfdg',
+//             year: 2007,
+//         },
+//     },
+// }
+
+// console.log(person);
+// console.log(person.edu.secondary.year);
+
+
+// const a = 45
+// const b = 'fvgv'
+
+// console.log(typeof null);
+
+// const str = '123fj5'
+// console.log(str, typeof str);
+
+// const numStr1 = Number(str)
+// console.log(numStr1, typeof numStr1);
+
+// const numStr2 = +str
+// console.log(numStr2, typeof numStr2);
+
+// const numStr3 = parseInt(str)
+// console.log(numStr3, typeof numStr3);
+
+// const str = false
+// console.log(str, typeof str);
+
+// const numStr1 = Number(str)
+// console.log(numStr1, typeof numStr1);
+
+// const numStr2 = +str
+// console.log(numStr2, typeof numStr2);
+
+// const numStr3 = parseInt(str)
+// console.log(numStr3, typeof numStr3);
+
+// const str = '123fj5'
+// console.log(str, typeof str);
+
+// const boolStr1 = Boolean(str)
+// console.log(boolStr1, typeof boolStr1);
+
+// const boolStr2 = !!str
+// console.log(boolStr2, typeof boolStr2);
+
+// const num = NaN
+// console.log(num, typeof num);
+
+// const boolnum1 = Boolean(num)
+// console.log(boolnum1, typeof boolnum1);
+
+// const boolnum2 = !!num
+// console.log(boolnum2, typeof boolnum2);
+
+
+const num = 123
+console.log(num, typeof num);
+
+const strnum1 = num + 'hfjh'
+console.log(strnum1, typeof strnum1);
+
+const strnum2 = num.toString()
+console.log(strnum2, typeof strnum2);
+
+const strnum3 = String(num)
+console.log(strnum3, typeof strnum3);
+
+
+
+// const a = prompt()
+// console.log();
+
+// const b = 1
+// console.log(a+b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
