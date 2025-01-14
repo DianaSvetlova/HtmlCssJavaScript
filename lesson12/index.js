91e09d939efc87647c6107e8a4ae3d77b3aad728
DianaSvetlova <dianasvetlova26@gmail.com>
2025-01-14 22:01:33 +0300

    js methods if

---
+++ lesson12/index.js
@@ -0,0 +1,169 @@
+const str = '    SOME random STrinG'
+const str2 = 'next string'
+
+// console.log(str.toLocaleUpperCase());
+// console.log(str.toLocaleLowerCase());
+// console.log(str);
+
+// const strUp = str.toLocaleUpperCase()
+// console.log(strUp);
+
+// console.log(str.concat(' ',str2,123));
+// console.log(str);
+
+// console.log('50'+25);
+// console.log('50'-25);
+
+// console.log(str.repeat(5));
+// console.log(str);
+
+// console.log(str.includes('str'));
+
+// console.log(str.toLowerCase().includes('str'));
+
+// const strLow = str.toLocaleLowerCase()
+// const isHas = strLow.includes('str')
+// console.log(isHas);
+
+// console.log(str.startsWith('SOME'));
+// console.log(str.endsWith('string'));
+
+// console.log(str.replace(/s/gi, '$').replace('a','@'));
+// console.log(str);
+
+// console.log(str.replaceAll('S', '$'));
+// console.log(str);
+
+// console.log(str.charAt(6));
+
+// console.log(str.indexOf('ran'));
+// console.log(str.lastIndexOf('S'));
+
+// console.log(str.slice(5,11));
+// console.log(str);
+
+
+// console.log(str.substring(5,11));
+// console.log(str);
+
+// console.log(str.substr(5,6));
+// console.log(str);
+
+// console.log(str.trim());
+// console.log(str);
+// console.log(str.trimStart());
+// console.log(str.trimEnd());
+
+
+// const arr = [1, 'two', null, false, {a:2}, [1,2,3]]
+// console.log(arr);
+
+// const obj = arr[4]
+// console.log(obj);
+
+// console.log(arr[5][1]);
+
+// const arr = [0,1,2,3,4]
+// const arr2 = [5,6,7,8,9]
+
+// console.log(arr.concat(arr2, 111, 'jhg,g'));
+
+// arr.push('five', 'six', 'seven', 'eight')
+// console.log(arr);
+// console.log(arr.push('five', 'six', 'seven', 'eight'));
+// console.log(arr);
+
+// arr.unshift('five', 'six', 'seven', 'eight')
+// console.log(arr);
+// console.log(arr.unshift('five', 'six', 'seven', 'eight'));
+
+// const el = arr.pop()
+// console.log(arr);
+// console.log(el);
+
+// const el = arr.shift()
+// console.log(arr);
+// console.log(el);
+
+// console.log('string'.indexOf('std'));
+
+// console.log(arr.reverse());
+// console.log(arr.toReversed());
+// console.log(arr);
+
+// console.log('arr',arr);
+
+
+// const arrSlice = arr.slice(1,4)
+// console.log('arrSlice',arrSlice);
+// console.log('arr',arr);
+
+// const arrSplice = arr.splice(1,0,11,22,33)
+// console.log('arrSplice',arrSplice);
+// console.log('arr',arr);
+
+
+
+// const age = 18
+
+// if(age>18) {
+//     console.log('>18');  
+// } else if (age<18) {
+//     console.log('<18');    
+// } else {
+//     console.log('=18');
+// }
+
+// if (age>18) {
+//     console.log('>18');
+// } else if (age<18) {
+//     console.log('<18');
+// }
+// else if (age<19) {
+//     console.log('<19');
+// }
+// else if (age<20) {
+//     console.log('<20');
+// }
+// else if (age<21) {
+//     console.log('<21');
+// }
+// else if (age<22) {
+//     console.log('<22');
+// }
+
+
+// const name = 'john'
+// if (name !== 'john') {
+//     console.log('go');
+// } 
+
+// console.log(93122%2 ===0);
+
+const a = 45
+const b = 78
+const name = john
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
