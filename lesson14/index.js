488eeba9bf06183039f43a2e70140f87a9a9a6c2
DianaSvetlova <dianasvetlova26@gmail.com>
2025-01-21 21:23:38 +0300

    function

---
+++ lesson14/index.js
@@ -0,0 +1,251 @@
+// let str = 'SOME random stRinG'
+
+// const arr = str.split(' ')
+// console.log(arr);
+
+// arr[1] = arr[1].toUpperCase()
+// console.log(arr);
+
+// str = arr.join('__')
+// console.log(str);
+
+
+// function sayHi (userName) {
+//     console.log(`hi, ${userName}!`);
+// }
+
+// sayHi('dina')
+// sayHi('diana')
+
+// const login = prompt()
+// sayHi(login)
+
+
+// function sum(a,b) {
+//     console.log('a+b=', a+b)
+    
+// }
+
+// sum(10,20)
+// sum(10,2000)
+
+// function math(a,b,c) {
+//     console.log('a+b*c=', a+b*c)
+    
+// }
+
+// math(10,20,3)
+// math(20,3,10)
+// math(10,3,20)
+
+// function f(a,b,...ostatok) {
+//     console.log(a,b);
+//     // console.log(arguments);
+//     console.log(ostatok);
+    
+// }
+
+// f(1,2,3,454,878)
+
+
+// function f(...rest) {
+//     console.log(rest);
+//     console.log(...rest);
+    
+// }
+
+// f(1,2,3,454,878)
+
+// console.log(...[1,2,3,4,5]);
+
+// const arr = [1,2,3]
+// const arr2 = [4,5,6]
+
+// // arr.push(arr2)
+// // console.log(arr);
+
+// arr.push(...arr2)
+// console.log(arr);
+
+
+// function sum(a,b) {
+//     return a+b
+    
+// }
+
+// console.log(sum(10,20));
+
+// const sumVar = sum(2,4)
+// console.log(sumVar);
+
+// const sum = function (a=3,b = 5) {
+//     console.log(a);
+//     console.log(b);
+//     return a+b
+// }
+
+// // console.log(sum(1,2));
+// // console.log(sum(2));
+// // console.log(sum(1,2));
+// console.log(sum());
+
+
+// const sum = (a,b=0) => {
+//     return a+b
+// }
+
+// console.log(sum(11,8));
+// console.log(sum(2));
+
+// const sum = (a,b=0) =>  a+b 
+
+// console.log(sum(11,8));
+// console.log(sum(2));
+
+// sum(10,20)
+
+// function sum(a,b) {
+//     console.log('a+b=', a+b)
+    
+// }
+
+
+// function () {
+//     console.log('hi!');
+    
+// }
+
+
+
+// (function () {
+//     console.log('hi!');
+    
+// }) ()
+
+
+// function hello () {
+//     console.log('hi!');
+    
+// }
+
+// hello()
+
+// let str = 'qwErty'
+
+// function strUp (str) {
+
+//     let arr = str.split('')
+//     console.log(arr);
+    
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if (i%2===0) {
+//             arr[i] = arr[i].toLowerCase()
+//         } else {
+//             arr[i] = arr[i].toUpperCase()
+//         }
+
+//     }
+
+//     return arr.join('') 
+    
+// }
+
+// str = strUp (str)
+
+// console.log(str);
+
+
+// const a = 5
+// console.log('doc', a);
+
+// // if (true) {
+// //     const a=6
+// //     const b=6
+// //     console.log('if',a);
+// //     console.log('if',b);
+// // }
+
+// // console.log('doc', b);
+
+// for (let i = 0; i < 1; i++) {
+
+//     console.log('for',a);
+//     const c =7
+//     console.log('for',c);
+    
+
+//     if (true) {
+//             const a=6
+//             const b=6
+//             console.log('if',a);
+//             console.log('if',b);
+//             console.log('if',c);
+
+//         }
+// }
+
+
+// const people =[
+//     {name: 'john', age: 20},
+//     {name: 'tania', age: 16},
+//     {name: 'sania', age: 30},
+//     {name: 'ola', age: 5}
+// ]
+
+// function isAdult(arr) {
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         // let arr2 = []
+
+//         if (arr[i].age > 18) {
+//             // arr2 = arr2.concat(arr[i].name)  
+//             arr[i] = arr[i].name        
+//         } 
+//     }
+
+//     // return arr2
+
+
+// }
+
+// isAdult(people)
+
+
+// const people = [
+//   { name: "John", age: 20 },
+//   { name: "Jane", age: 16 },
+//   { name: "Jessica", age: 25 },
+// ];
+// function isAdult(arr) {
+//   const names = [];
+
+//   for (const element of arr) {
+//     if (element.age >= 18) {
+//       names.push(element.name);
+//       // console.log(element.name);
+//     }
+//   }
+//   return names;
+// }
+// console.log(isAdult(people));
+
+// isAdult(people);
+
+// const number = 1867654
+
+// function sum (num) {
+
+//     const str=String(num)
+//     console.log(str);
+
+
+
+//     for 
+    
+
+// }
+
+// sum(1867654)
