f546ae77bc5998e185e9cb1bd7b5f1240f1363ec
DianaSvetlova <dianasvetlova26@gmail.com>
2025-01-22 18:53:27 +0300

    function

---
+++ lesson14/index1.js
@@ -0,0 +1,76 @@
+// function sayHi (userName) {
+//     console.log(`hi ${userName}`);
+    
+// }
+
+// sayHi ('dina')
+// sayHi ('diana')
+
+// const login = prompt()
+// sayHi (login)
+
+
+// function math(a,b,c) {
+//     console.log(a+b*c);
+    
+// } 
+// math(10,78,1)
+
+// function f(a, ...rest) {
+//     console.log(a);
+    
+//     console.log(rest);
+     
+// } 
+// f(10,78,1,48,482)
+
+// function f(...aaaa) {
+//     console.log(aaaa);
+//     console.log(...aaaa);
+    
+// }
+// f (5,57454,7454,652,875)
+
+// console.log(...[545,875,6232,45]);
+
+// const arr = [1,2,3]
+// const arr2 = [4,5,6]
+
+// arr.push(...arr2)
+// console.log(arr);
+
+// console.log(arr.concat(arr2));
+
+
+// function sum(a,b){
+//     return a+b
+// }
+// // console.log(sum (12,45));
+
+// const sumVar = sum (12,78)
+// console.log(sumVar);
+
+
+// const sum = function(a,b=5){
+//     console.log(b);
+//     return a+b
+// }
+
+// console.log(sum(12));
+
+
+//  
+
+// const sum = (a,b) =>  a+b 
+// console.log(sum(2,6));
+
+
+// (function () {
+//     console.log('hi');
+    
+// }) ()
+ 
+
+
+
+
