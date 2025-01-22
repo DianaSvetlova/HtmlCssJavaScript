150e72f06961c5e87efe9baf74bf944c4e45ba10
DianaSvetlova <dianasvetlova26@gmail.com>
2025-01-22 14:09:56 +0300

    dz13

---
+++ dz13/dz13.js
@@ -0,0 +1,114 @@
+// task1
+
+// let arr = [12,2589,657,12,958,657,657,12589,12,2]
+// let arr2 = []
+
+// arr = arr.sort()
+
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     if (arr[i]!==arr[i+1]) {
+//         arr2 = arr2.concat(arr[i])
+//     }
+// }
+// console.log(arr);
+// console.log(arr2);
+
+
+// task2
+   
+    // let fact = 8 
+    // let length = 8 
+   
+    // for (i=1; i<(length-1); i++) {
+
+    //     fact = fact*(length-i)
+    // }
+    // console.log(fact);
+
+// task3
+
+// const year = 1988
+
+// if (year%4===0 && year>=0) {
+//     console.log('leap year');
+// } else {
+//     console.log('not a leap year');
+// }
+
+
+// task4
+
+// let arr = [12,2589,657,12,958,657,657,12589,12,2]
+// let arr2 = []
+
+// arr = arr.sort()
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     if (arr[i]!==arr[i+1]) {
+//         arr2 = arr2.concat(arr[i])
+//     }
+// }
+// console.log(arr2);
+// console.log(arr2.length);
+
+
+// task5
+
+// const person = {
+//     name: 'dina',
+//     age: 36,
+//     gender: 'female',
+//     hands: {
+//               first: 'left',
+//               second: 'right',
+//             },
+//     }
+
+// const keys = Object.keys(person)
+// console.log(keys);
+// console.log(keys.length);
+
+
+// task6
+
+// const party =[    
+//     {name:'jenek',
+//     age: 37},
+//     {name:'yarik',
+//     age: 29},
+//     {name:'sania',
+//     age: 35},
+//     {name:'rostik',
+//     age: 40},
+//     {name:'dasha',
+//     age: 18},
+// ]
+
+// console.log(party);
+
+// let partyAge =[]
+
+// for (let i = 0; i < party.length; i++) {
+
+//     if (party[i].age>30) {
+//         partyAge = partyAge.concat(party[i].name)
+//     }
+// }
+
+// console.log(partyAge);
+
+
+// task7
+
+// let arr =[]
+// const needLength = 10
+
+// while 
+// (arr.length < needLength) {
+//     let i = Math.floor(Math.random() * 101)
+//     arr = arr.concat(i)  
+// }
+//     console.log(arr);
