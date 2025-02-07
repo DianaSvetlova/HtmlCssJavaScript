1b98fd28454f1d2fd18b4df9839f0b0ff2416ffc
DianaSvetlova <dianasvetlova26@gmail.com>
2025-02-07 18:18:23 +0300

    dz15

---
+++ dz15/dz15.js
@@ -0,0 +1,125 @@
+// task 1a
+
+// function arithmeticMean (arr) {
+
+// const arrEven = arr.filter(el => el%2===0)
+// console.log(arrEven);
+
+// const sum = arrEven.reduce((acc,el) => (acc+el), 0)
+
+// const arithmeticMean = sum/arrEven.length
+// console.log(arithmeticMean);
+// }
+
+// arithmeticMean ([258,456,0,75,135,-89,87,-13,0,7896,0,-3219])
+
+
+// task 1b
+
+// function sr (arr) {
+//     const arrPositive = arr.filter(el => el>0)
+//     console.log(arrPositive);
+// }
+// sr ([258,456,0,75,135,-89,87,-13,0,7896,0,-3219])
+
+
+
+// task 1c
+
+// function summa3 (arr) {
+//     const sum = arr.filter(el => el%3===0).reduce((acc,el) => acc+el, 0)
+//     console.log(sum);
+// }
+// summa3 ([258,456,0,75,135,-89,87,-13,0,7896,0,-3219])
+
+
+// task 2
+
+// function nearMiddle (arr) {
+    
+// const sum = arr.reduce((acc,el) => (acc+el), 0)
+
+// const arithmeticMean = sum/arr.length
+// console.log(arithmeticMean);
+
+// arr.push(arithmeticMean)
+
+// arr.sort((a,b) => (a-b))
+// console.log(arr);
+
+// const iArithmeticMean = arr.findIndex(el => el===arithmeticMean) 
+
+// console.log(iArithmeticMean);
+
+// if ((arr[iArithmeticMean+1]-arr[iArithmeticMean])<=(arr[iArithmeticMean]-arr[iArithmeticMean-1])) {
+//         console.log(arr[iArithmeticMean+1]);} else {
+//         console.log(arr[iArithmeticMean-1]);
+//     }
+// }
+
+// nearMiddle ([258,456,0,75,135,-89,87,-13,0,7896,0,-3219])
+
+
+// task 3
+
+// function sumMiddle (arr) {
+
+// const firstZero = arr.indexOf(0)
+// const lastZero = arr.lastIndexOf(0)
+
+// const sum = arr.reduce(function (acc, el, i) {
+//     if (i>firstZero && i<lastZero) {
+//         acc += el
+//     }
+//     return acc
+// }, 0)
+
+// console.log(sum);
+
+// }
+// sumMiddle ([21,0,25,0,-5,36,0,98])
+
+
+// task 4
+
+// const arr = [21,0,25,0,-5,36,0,98]
+
+
+// function move (n) {
+
+// for (i=0; i<n; i++) {
+    
+//     arr.unshift(arr.pop());
+// }
+// console.log(arr);
+// }
+
+// move (4)
+
+
+// task 5
+
+// let arr = [1, 3, 4, 1, 1, 1, 1, 1, 3, 4, 5];
+// let count = {};
+
+// for (let elem of arr) {
+//     if (count[elem] === undefined) {
+//     count[elem] = 1;
+//     } else {
+//     count[elem]++;
+//     }
+// }
+// console.log(count);
+
+// const arrCount = Object.values(count)
+// console.log(arrCount);
+
+// let maxCount = arrCount[1]
+
+// for (i=0; i<arrCount.length; i++) {
+//     if(arrCount[i]>maxCount) {
+//         maxCount = arrCount[i]
+//     }
+// }
+
+// console.log(maxCount);
